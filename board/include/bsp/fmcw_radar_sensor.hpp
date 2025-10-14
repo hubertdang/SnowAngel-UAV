be5320a76b7d3497fac7d3a320299b6391cef760
Karran Dhillon <karrandhillon@cmail.carleton.ca>
2025-10-14 11:34:33 -0400

    FMCW Radar Simulation Stubs (#19)

--- board/include/bsp/fmcw_radar_sensor.hpp
+++ board/include/bsp/fmcw_radar_sensor.hpp
@@ -1,12 +1,12 @@
 /**
- * 
+ *
  * Name: sensor.hpp
  * Author: Karran Dhillon
- * 
+ *
  * This file describes the public interface to the sensor bsp layer.
- * 
+ *
  * Date: October 2025
- * 
+ *
  * Copyright 2025 SnowAngel-UAV
  */
 
@@ -15,28 +15,41 @@
 
 #include <cstdint>
 
+//----------------------------------------------------------------
+#define FMCW_RADAR_FFT_SIZE 2048
+
 typedef struct fmcw_waveform_data
 {
-    // TODO Karran: change after deciding on waveform data structure
-    uint8_t raw_data[256];
+// Size Calculation:
+// '{"FFT":['                                                 //   8
+// FMCW_RADAR_FFT_SIZE samples, each 2 decimal digits + comma // + 6*FMCW_RADAR_FFT_SIZE
+// ']}\0'                                                     // + 3
+#define FMCW_RADAR_MAX_DATA_SIZE (8 + (6 * FMCW_RADAR_FFT_SIZE) + 3)
+
+	uint8_t raw_data[FMCW_RADAR_MAX_DATA_SIZE];
+	uint16_t fft_size;
 } fmcw_waveform_data_t;
+
+//----------------------------------------------------------------
 
 class FMCW_RADAR_SENSOR
 {
 public:
-    // the following functions describe the public interface.
-    // CAUTION! CAUTION! DO NOT describe the state or implementation of the sensor. 
-    // CAUTION! CAUTION! Please be wary if deciding to change the interface. You
-    //                   risk exposing extra information to the application code.
+	// the following functions describe the public interface.
+	// CAUTION! CAUTION! DO NOT describe the state or implementation of the sensor.
+	// CAUTION! CAUTION! Please be wary if deciding to change the interface. You
+	//                   risk exposing extra information to the application code.
 
-    // Pure virtual functions enforce child class implementations
-    virtual int8_t fmcw_radar_sensor_init()  = 0;
-    virtual int8_t fmcw_radar_sensor_start_tx_signal() = 0;
-    virtual int8_t fmcw_radar_sensor_read_rx_signal(fmcw_waveform_data_t *data) = 0;
-    virtual int8_t fmcw_radar_sensor_stop_tx_signal()  = 0;
+	// Pure virtual functions enforce child class implementations
+	virtual int8_t fmcw_radar_sensor_init() = 0;
+	virtual int8_t fmcw_radar_sensor_start_tx_signal() = 0;
+	virtual int8_t fmcw_radar_sensor_read_rx_signal(fmcw_waveform_data_t *data) = 0;
+	virtual int8_t fmcw_radar_sensor_stop_tx_signal() = 0;
 
-    virtual ~FMCW_RADAR_SENSOR() {}
-    // do not declare anything as private or protected
+	virtual ~FMCW_RADAR_SENSOR() {}
+	// do not declare anything as private or protected
 };
+
+//----------------------------------------------------------------
 
 #endif // #ifndef FMCW_RADAR_SENSOR_H
--- board/src/bsp/ops_fmcw.hpp
+++ board/src/bsp/ops_fmcw.hpp
@@ -15,11 +15,18 @@
 
 #include "bsp/fmcw_radar_sensor.hpp"
 #include <cstdint>
+#include <string>
 
 class OPS_FMCW : public FMCW_RADAR_SENSOR
 {
 public:
-	OPS_FMCW(uint8_t usb_port);
+	static FMCW_RADAR_SENSOR *get_fmcw_radar_instance(const uint8_t usb_port);
+
+	// OPS_FMCW should not be cloneable.
+	OPS_FMCW(OPS_FMCW &other) = delete;
+
+	// OPS_FMCW should not be assignable.
+	void operator=(const OPS_FMCW &) = delete;
 
 	int8_t fmcw_radar_sensor_init() override;
 	int8_t fmcw_radar_sensor_read_rx_signal(fmcw_waveform_data_t *data) override;
@@ -28,6 +35,16 @@ public:
 	~OPS_FMCW() override {}
 
 private:
+	// Constructor is private to enforce factory function usage.
+	// Follows the singleton design pattern.
+	OPS_FMCW(uint8_t usb_port);
+
+	// helper functions
+	int8_t send_command(const char *cmd);
+	int8_t read_response(std::string *response);
+
+private:
+	static OPS_FMCW *instance;
 	uint8_t usb_port;
 };
 
