ff3ad55bd7258c52ab780e1b3f78ed73d391bf58
Karran Dhillon <karrandhillon@cmail.carleton.ca>
2025-11-08 11:41:11 -0500

    remove verbose printf

--- board/unittest/test_adafruit_tm117.cpp
+++ board/unittest/test_adafruit_tm117.cpp
@@ -27,7 +27,6 @@ int main()
     // Test reading temperature
     temp_sensor_data_t temperature_data;
     int8_t read_result = sensor->temperature_sensor_read(&temperature_data);
-    printf("Temperature: %.2f C\n", temperature_data.temperature);
     assert(read_result == 0);
     assert(temperature_data.temperature > -40.0f && temperature_data.temperature < 125.0f); // Assuming valid range for TMP117
 
